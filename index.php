eb4cbb5ebb244126f24a060b1268c30fe8a769eb
Save <66020327+Save9253@users.noreply.github.com>
2020-09-03 18:50:20 -0400

    home page content

--- index.php
+++ index.php
@@ -1,8 +1,83 @@
 <?php
     $page = 'Home';
     $HeaderLogo = 'yes';
+    $styles = '<link rel="stylesheet" href="./styles/home.css">';
     include_once './parts/header.php';
 ?>
-
-<form action="./functions/pwdRstMail.php"><button type="submit">Send</button></form>
+<section id="intro">
+    <p>Nowadays, a website is a very important part of a business's image. It is often the first impression your customers get, and we all know how important it is to make the right first impression. </p>
+    <p>Making a website on your own takes a lot of time and energy. You might make mistakes and lose your time, money, and reputation due to bad customer experiences on your website.</p>
+    <p>Thankfully, I am here so that you don't have to go through the painful but important process of making a website on your own. Scroll down to see how I can make your life easier on every step of making a website.</p>
+</section>
+<section class="compare topS">
+    <h1>Finding a domain:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>You will have to compare among at least a dozen of domain providers to compare their prices. Knowing what is going to be the best option for you isn't an easy task considering all of their deceiving strategies. Most of the companies who sell domains show a lower than average price for the first year while hiding a smaller price tag for the next year renewal somewhere in a corner. Plus, how do you know what will work best for you? Would .org, .com, .online, or something else be the best choice?</p>
+    </div>
+    <div class="right good">
+        <h2>with Save</h2>
+        <p>I'll direct you through the process of finding the best domain for you from the best trusted source.</p>
+    </div>
+</section>
+<section class="compare">
+    <h1>Hosting a website:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>Hosting a website is a pretty similar process to finding a reliable and trustworthy domain provider. So, it can be pretty hard. Some hosting companies like to show their price lower than anyone else's, even if it involves showing only part of the actual price. Moreover, you will be able to get such a price only if you pay for the whole year in advance. </p>
+    </div>
+    <div class="right good">
+        <h2>with Save</h2>
+        <p>No hosting companies will be involved and no extra money will be paid. It will be solely your website and a Google Server at truly the lowest price for hosting. You can choose to pay monthly or annually, with no additional charge for monthly payments. </p>
+    </div>
+</section>
+<section class="compare">
+    <h1>Content Management System:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>Content Management Systems (CMS) are designed to meet the needs of everyone, even though everyone has very different needs. So, be ready to deal with an overwhelming amount of settings and buttons that you might not even need. Even with all those tools, you will still have to add some code to make things look or function exactly the way you want. Moreover, every CMS that is designed for all possible needs has a really long code to do so. Using all the unnecessary code slows down your website and, in turn, makes you rate lower on Google.</p>
+    </div>
+    <div class="right good">
+        <h2>with Save</h2>
+        <p>Honestly, not everyone even needs a Content Management System. And if you do need one, it should be designed for your specific needs. Such CMS will make it easier for you to manage your content and it will make your site load faster which, consequently, will positively affect your ranking on Google.</p>
+    </div>
+</section>
+<section class="compare">
+    <h1>Layout:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>Content Management Systems usually have free templates available. Nevertheless, the options are usually pretty limited.  So, if you do want an impressive and unique layout that fits your needs perfectly, be ready to pay extra. The most popular scenario is using WordPress with Divi theme which will cost you at least $70 extra a year.</p>
+    </div>
+    <div class="right good">
+        <h2>with Save</h2>
+        <p>There are no limits on how the layout can look. I will build a special design just for you and your needs. I'll create your website according to your preferred style. Or, if you don't have a particular style in mind, I'll gladly compose one for you! </p>
+    </div>
+</section>
+<section class="compare">
+    <h1>Functionality:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>A similar problem happens with functionality. On WordPress, for example, only a blog is free. But if you want a store, you will have to get an extra plugin. If you want a form, you will have to get an extra plugin. Every plugin will ask you to pay extra for their services. Even worse, every plugin will slow down your website more and more.</p>
+    </div>
+    <div class="right good">
+        <h2>with Save.</h2>
+        <p>I will bring to reality any functionality you may need with as little code as possible to ensure the best performance. </p>
+    </div>
+</section>
+<section class="compare bottS">
+    <h1>Search Engine Optimization:</h1>
+    <div class="left bad">
+        <h2>on your own</h2>
+        <p>First, you will have to deal with Google Search Console to just appear on Google. Then, you will still have to make sure that every piece of information on your website is helping you to rank higher on Google and not working against you.</p>
+    </div>
+    <div class="right good">
+        <h2>with Save</h2>
+        <p>I will make sure you appear on Google and give you all the recommendations on how to rank higher and be found by your customers.</p>
+    </div>
+</section>
+<section id="conclusion">
+    <p>It is your choice in the end. </p>
+    <p>You can make a website on your own, paying seemingly small prices to greedy large corporations, who are in business just to make as much money as they can. Additionally, you can risk your reputation on the web, lose precious time, and end up with a slower website.</p>
+    <p>You can trust making a website to me, a hard-working entrepreneur who is passionate about helping other business owners succeed by building excellent websites.  I will make a website just the way you want and consider all of your needs. Your website will load fast and give the best experience to your customers. You won't have to worry about anything. I will take care of every step of the process and will keep you updated with regular reports. </p>
+</section>
 <?php include_once './parts/footer.php'?>
