a0210f9b42889ad0364d49625db3c1d0d4423f85
Save <66020327+Save9253@users.noreply.github.com>
2020-09-09 19:23:29 -0400

    good stile found for menu focus and hover

--- parts/header.php
+++ parts/header.php
@@ -18,12 +18,15 @@
         <ul>
             <?php if($page !== 'Login'):?>
             <li>
-                <a href="login.php">
+                <a class="menuLink" href="<?php if(isset($_SESSION['uname'])){echo 'functions/logout.php';}else{echo 'login.php';}?>">
                     <svg width="30" height="30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M28.7239 1.34717H2.73124C1.62464 1.34717 0.727539 2.24421 0.727539 3.35081V29.3435C0.727539 30.4501 1.62464 31.3471 2.73124 31.3471H28.7239C29.8305 31.3471 30.7276 30.4501 30.7276 29.3435V3.35081C30.7276 2.24421 29.8305 1.34717 28.7239 1.34717Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M13.5923 6.16443L10.5221 8.52802L9.89853 11.1735L10.0416 15.1178L9.60826 15.3306L9.38756 16.5854L9.98751 18.3216L10.7208 19L11.2406 18.9591L11.872 20.3348L12.1257 20.6075L12.1597 24.2408L4.32238 28.2314L3.04852 29.7744L2.81464 31.3472H28.7465L28.5389 29.1973L27.3564 27.5271L19.6171 24.0751L19.3443 21.1964L19.715 20.7072L20.4594 19.1713L20.9841 19.1156L21.4458 18.2966L22.0402 16.5636L21.7264 15.2978L21.2489 15.2788L21.5646 13.3565L21.4862 10.8461L20.6053 8.31604L17.5891 6.30194L13.5923 6.16443Z" fill="black"/>
+                        <path d="M28.7 1.3H2.7C1.6 1.3 0.7 2.2 0.7 3.2V29.3C0.7 30.5 1.6 31.3 2.7 31.3H28.7C29.8 31.3 30.7 30.5 30.7 29.3V3.2C30.7 2.2 29.8 1.3 28.7 1.3Z" stroke="var(--dr)" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path
+                            d="M13.6 6.2L10.5 8.5L9.89853 11.2L10 15.1L9.6 15.3L9.2 16.6L10 18.3L10.7 19L11.2 19L11.872 20.3L12.1 20.6L12.2 24.2L4.3 28.2L3 29.8L2.8 31.3H28.7L28.5 29.2L27.2 27.5L19.6 24.1L19.3 21.2L19.7 20.7L20.5 19.2L21 19.1L21.4 18.3L22 16.6L21.7 15.3L21.2 15.3L21.6 13.2L21.5 10.8L20.6 8.3L17.6 6.3L13.6 6.2Z"
+                            <?php if(isset($_SESSION['uname'])){echo 'fill="var(--dr)"';}else{echo 'stroke="var(--dr)" stroke-linecap="round" stroke-linejoin="round"';}?>
+                        />
                     </svg>
-                <?php #if(isset($_SESSION['uname'])){echo $_SESSION['uname'];}else{echo 'Log in';}?>
+                    <div class="menuTtl">Login</div>
                 </a>
             </li>
             <?php endif;?>
