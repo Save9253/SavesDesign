1de8ecb837af0efea6af9136621fc313e8f7b620
Save <66020327+Save9253@users.noreply.github.com>
2020-09-22 20:39:57 -0400

    side menu marks but I don't like them

--- scripts/secNav.js
+++ scripts/secNav.js
@@ -2,13 +2,14 @@ const nav = document.querySelector('.secNav');
 const h2s = document.querySelectorAll('.secWNav h2');
 const header = document.querySelector('header');
 const headerEls = document.querySelectorAll('header > *');
+const navM = '<div class="navM"><svg  width="10" height="5" viewBox="0 0 10 5" fill="var(--md)" xmlns="http://www.w3.org/2000/svg"><path d="M0 0H10V2.5L5 5L0 2.5Z"/></svg><svg class="crcl" width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><circle cx="5" cy="5" r="9"/></svg><svg width="10" height="5" viewBox="0 0 10 5" fill="var(--md)" xmlns="http://www.w3.org/2000/svg"><path d="M0 0L5 2.5L10 0V5H0Z"/></svg></div>';
 
 h2s.forEach(h2 => {
     const anch = h2.textContent.replaceAll(' ','-');
     const newA = document.createElement('a');
     h2.setAttribute('id', anch);
     newA.setAttribute('href','#' + anch);
-    newA.innerHTML = h2.textContent;
+    newA.innerHTML = '<span>'+ h2.textContent + '</span>' + navM;
     nav.appendChild(newA);
     newA.addEventListener('click', ()=>{
         header.style.top = '-55px';
